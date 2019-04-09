aa3d64f1101b72966aab18740f01f52560ce1615
mzuccaroli <marcozuccaroli@gmail.com>
2019-04-09 17:25:16 +0200

    specific url redirects implemetation

--- server.js
+++ server.js
@@ -2,9 +2,7 @@
 const express = require("express");
 const compression = require("compression");
 const request = require("request");
-const mcache = require('memory-cache');
 
-const _environment = process.env.ENVIRONMENT || 'develop';
 const _bucketAddress = 'http://static.test.com.s3-website-eu-west-1.amazonaws.com';
 const _apiAddress = 'https://testapi/api/example/sitemap';
 const _port = 4100;
@@ -12,63 +10,6 @@ const _port = 4100;
 const app = express();
 app.use(compression());
 
-// ---- MEMORY CACHE ---- //
-const cache = {
-    getCache: () => {
-        return (req, res, next) => {
-            let key = '__express__' + req.originalUrl;
-            let cachedBody = mcache.get(key);
-            if (cachedBody) {
-                res.type(cachedBody.headers['content-type']);
-                res.send(cachedBody.body);
-            } else {
-                next()
-            }
-        }
-    },
-
-    handleResponse: (req, res, url) => {
-        request(url, function (error, response) {
-            let key = '__express__' + req.originalUrl;
-            mcache.put(key, response, 120000); //2 min
-        }).pipe(res);
-    }
-};
-
-// ---- LIMIT FOR TOO MANY CONNECTIONS ---- //
-if (_environment === 'production') {
-    const rateLimit = require("express-rate-limit");
-    const limiter = rateLimit({
-        windowMs: 10000,
-        max: 200,
-        message: "Too many requests from this IP, please try again"
-    });
-    app.use(limiter);
-}
-
-// ---- REDIRECT TO HTTPS ---- //
-if (_environment === 'production') {
-    app.enable('trust proxy');
-    app.use(function (req, res, next) {
-        if (req.secure) {
-            next(); // request was via https, so do no special handling
-        } else {
-            res.redirect(301, 'https://' + req.headers.host + req.url); // request was via http, so redirect to https
-        }
-    });
-}
-
-// ---- REDIRECT NON-WWW REQUESTS ---- //
-if (_environment === 'production') {
-    app.get('/*', function (req, res, next) {
-        if (req.headers.host.match(/^www/) == null) {
-            // req.headers.host = "www." + req.headers.host;
-            res.redirect('https://www.' + req.headers.host + req.url);
-        } else {
-            next();
-        }
-    });
-}
 
 // ---- SERVE SITEMAPS.XML FROM A DEDICATED API ---- //
 app.all('*.xml', cache.getCache(), function (req, res) {
@@ -83,17 +24,18 @@ app.all('*.xml', cache.getCache(), function (req, res) {
 });
 
 // ---- SERVE STATIC FILES FROM A BUCKET ---- //
-app.all('*.(js|css|ttf|svg|png|jpg|jpeg|ico|woff2|woff|txt|html)', cache.getCache(), function (req, res) {
+app.all('*.(js|css|ttf|svg|png|jpg|jpeg|ico|woff2|woff|txt|html)', function (req, res) {
     const url = _bucketAddress + req.url;
-    cache.handleResponse(req, res, url);
+    request(url).pipe(res);
 });
 
 // ---- SERVE APLICATION PATHS FROM A BUCKET ---- //
 app.all('*', cache.getCache(), function (req, res) {
-    cache.handleResponse(req, res, _bucketAddress);
+    //here you can manage things like language folder for example: url = _bucketAddress+'/en'
+    request(_bucketAddress).pipe(res);
 });
 
 // ---- START UP THE NODE SERVER  ----
 app.listen(_port, function () {
-    console.log("Node Express server for " + app.name + " listening on https://localhost:" + _port);
+    console.log("Node Express server for " + app.name + " listening on http://localhost:" + _port);
 });
